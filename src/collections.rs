bcefb18a76f5554f1e721b8a2ce0fd40a5c5fe1e
Hannah Kolbeck <hskolbeck@gmail.com>
2022-11-02 15:32:25 -0700

    Standardize a collection internal function, more

--- src/collections.rs
+++ src/collections.rs
@@ -17,27 +17,27 @@ impl<T: Eq + Hash + Send + Sync> UpdatingSet<T> {
     }
 
     pub fn contains(&self, val: &T) -> bool {
-        match self.get_read_lock().as_ref() {
+        match self.get_collection().as_ref() {
             None => panic!("{}", NON_RUNNING),
             Some((_, h)) => h.contains(val)
         }
     }
 
     pub fn len(&self) -> usize {
-        match self.get_read_lock().as_ref() {
+        match self.get_collection().as_ref() {
             None => panic!("{}", NON_RUNNING),
             Some((_, h)) => h.len()
         }
     }
 
     pub fn is_empty(&self) -> bool {
-        match self.get_read_lock().as_ref() {
+        match self.get_collection().as_ref() {
             None => panic!("{}", NON_RUNNING),
             Some((_, h)) => h.is_empty()
         }
     }
 
-    fn get_read_lock(&self) -> Arc<Option<(u128, HashSet<T>)>> {
+    fn get_collection(&self) -> Arc<Option<(u128, HashSet<T>)>> {
         self.backing.read()
             .expect("Couldn't acquire lock on backing data structure")
             .clone()
@@ -59,28 +59,28 @@ impl<K: Eq + Hash, V> UpdatingMap<K, V> {
 
 impl<K: Eq + Hash + Send + Sync, V: Send + Sync> UpdatingMap<K, V> {
     pub fn get(&self, key: &K) -> Option<Arc<V>> {
-        match self.get_read_lock().as_ref() {
+        match self.get_collection().as_ref() {
             None => panic!("{}", NON_RUNNING),
             Some((_, h)) => h.get(key).cloned()
         }
     }
 
     pub fn len(&self) -> usize {
-        match self.get_read_lock().as_ref() {
+        match self.get_collection().as_ref() {
             None => panic!("{}", NON_RUNNING),
             Some((_, h)) => h.len(),
         }
     }
 
     pub fn is_empty(&self) -> bool {
-        match self.get_read_lock().as_ref() {
+        match self.get_collection().as_ref() {
             None => panic!("{}", NON_RUNNING),
             Some((_, h)) => h.is_empty(),
         }
     }
 
     #[allow(clippy::type_complexity)]
-    fn get_read_lock(&self) -> Arc<Option<(u128, HashMap<K, Arc<V>>)>> {
+    fn get_collection(&self) -> Arc<Option<(u128, HashMap<K, Arc<V>>)>> {
         self.backing.read()
             .expect("Couldn't acquire lock on backing data structure")
             .clone()
