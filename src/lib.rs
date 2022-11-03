93a0b2d1f27776952b6cd91f86190bf8470e8568
Hannah Kolbeck <hskolbeck@gmail.com>
2022-11-03 13:47:42 -0700

    Remove gcs/s3, as only async clients are currently available.

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,5 @@ pub mod processors;
 pub mod collections;
 pub mod metrics;
 
-pub mod s3;
+#[cfg(feature = "http")]
 pub mod http;
-pub mod gcs;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
